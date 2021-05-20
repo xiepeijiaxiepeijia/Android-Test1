db8b7da6afeea96d7b090e89a7b0d97e4bb78892
xiepeijiaxiepeijia <1312017475@qq.com>
2021-05-20 11:24:43 +0800

    add setText

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv_hello = findViewById(R.id.text1);
+        tv_hello.setText("hello world!");
+        tv_hello.setTextColor(Color.BLUE);
+        tv_hello.setTextSize(30);
     }
 }
